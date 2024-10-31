6838dce2c825974a96acd9b5ec028bdc98c4ec44
CynicalDoggo <157577893+CynicalDoggo@users.noreply.github.com>
2024-11-01 03:39:26 +0800

    Update create_tables.sql
    -Tweaks to guest, rooms and room_booking
    -added blacklist, sysadmin and staffmember

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,22 @@
+--Functions and variables 
 ALTER DATABASE postgres SET timezone TO 'Asia/Singapore';
+CREATE TYPE ROOMSTATUS AS ENUM('Available', 'Occupied', 'Maintenance');
+CREATE TYPE BEDTYPE AS ENUM('Twin', 'King', 'Queen');
 
+-- Create a trigger function to update the updated_at column
+CREATE OR REPLACE FUNCTION update_updated_at_column () RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = CURRENT_TIMESTAMP;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create a trigger that calls the function before each update
+CREATE TRIGGER update_staffmembers_updated_at BEFORE
+UPDATE ON StaffMembers FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column ();
+
+-- Guest Tables
 CREATE TABLE guest (
   guest_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   first_name TEXT NOT NULL,
@@ -7,6 +24,7 @@ CREATE TABLE guest (
   mobile_number TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
   password_hash TEXT NOT NULL,
+  faceId_Concsent BOOLEAN DEFAULT false,
   facial_data bytea
 );
 
@@ -15,20 +33,22 @@ CREATE TABLE room (
   room_number TEXT UNIQUE NOT NULL,
   room_type TEXT,
   is_smoking BOOLEAN,
-  is_available BOOLEAN DEFAULT TRUE
+  beds BEDTYPE,
+  status ROOMSTATUS DEFAULT 'Available',
+  price DECIMAL(10,2) NOT NULL
 );
 
 CREATE TABLE room_preference (
   guest_id BIGINT,
-  preference_type TEXT,
-  preference_value TEXT,
+  preferences TEXT,
   FOREIGN KEY (guest_id) REFERENCES guest (guest_id) ON DELETE CASCADE
 );
 
-CREATE TABLE room_reservation (
+CREATE TABLE room_booking (
   reservation_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   guest_id BIGINT,
   room_id BIGINT,
+  checkIn_Status BOOLEAN DEFAULT false,
   check_in_date TIMESTAMP WITH TIME ZONE,
   check_out_date TIMESTAMP WITH TIME ZONE,
   FOREIGN KEY (guest_id) REFERENCES guest (guest_id) ON DELETE CASCADE,
@@ -58,4 +78,34 @@ CREATE TABLE check_outs (
   timedate_out TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   facial_recognition_success BOOLEAN,
   FOREIGN KEY (guest_id) REFERENCES guest (guest_id) ON DELETE CASCADE
+);
+
+--Employee tables
+CREATE TABLE StaffMember (
+  staff_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  full_name TEXT NOT NULL,
+  phone_number TEXT NOT NULL,
+  email TEXT UNIQUE,
+  password_hash TEXT,
+  active_status BOOLEAN DEFAULT TRUE,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE SystemAdmins (
+  admin_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  full_name TEXT NOT NULL,
+  email TEXT UNIQUE,
+  password_hash TEXT,
+  active_status BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE Blacklist (
+  blacklist_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  guest_id BIGINT,
+  reason TEXT,
+  added_by BIGINT,
+  date_added TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (guest_id) REFERENCES guest (guest_id) ON DELETE CASCADE,
+  FOREIGN KEY (added_by) REFERENCES StaffMember (staff_id) ON DELETE SET NULL
 );
